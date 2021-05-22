0fd95079bbb70256de65777b6801c982ae11b5ce
sandesh <sdhawan12494@gmail.com>
2021-05-22 11:17:23 +0530

    Updated TC

--- src/test/java/SauceTests/CartTestCase.java
+++ src/test/java/SauceTests/CartTestCase.java
@@ -5,7 +5,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import SaucePages.CartPage;
 import Steps.CartPageSteps;
 import Steps.LoginPageSteps;
 import Steps.ProductsPageSteps;
